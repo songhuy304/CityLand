1a090fd2da3b5779cc6a74a792d0095f8c25c505
nghiaph <nghiaph1706@gmail.com>
2025-07-30 09:15:51 +0700

    feat: init wp link city theme

---
+++ wp-theme/link-city/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php wp_title('|', true, 'right'); ?><?php bloginfo('name'); ?></title>
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <header>
+        <!-- Dán phần header từ index.html -->
+        <!-- Ví dụ: Logo, menu -->
+        <nav>
+            <?php
+            wp_nav_menu(array(
+                'theme_location' => 'primary',
+                'menu_id' => 'primary-menu',
+            ));
+            ?>
+        </nav>
+    </header>
