83625944200c98059e5fe9b3f9b84b43e21a04a5
nghiaph <nghiaph1706@gmail.com>
2025-08-09 21:36:12 +0700

    feat: enhance detail page layout and implement video swiper functionality

--- wp-theme/link-city/page.php
+++ wp-theme/link-city/page.php
@@ -18,24 +18,22 @@ get_header(); ?>
                             <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
                         </header>
 
-                        <div class="entry-content">
-                            <?php
-                                the_content();
-
-                        wp_link_pages([
-                            'before' => '<div class="page-links">' . esc_html__('Pages:', 'link-city'),
-                            'after' => '</div>',
-                        ]);
-                        ?>
+                        <div class="swiper-arrow-2 style-2 prev arrow-outer prev" tabindex="0" role="button" aria-label="Previous slide" aria-controls="swiper-wrapper-6fa710823ded7e36a">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M15 4.5L7.5 12L15 19.5" stroke="#326768" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                            </svg>
                         </div>
-                    </article>
-                    <?php
-                    endwhile;
-?>
+
+                        <div class="swiper-arrow-2 style-2 next arrow-outer next" tabindex="0" role="button" aria-label="Next slide" aria-controls="swiper-wrapper-6fa710823ded7e36a"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M9 4.5L16.5 12L9 19.5" stroke="#326768" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                            </svg>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
+    </div>
 </div>
+<!-- ============================ END CONTENT ============================ -->
 
 <?php get_footer(); ?>
